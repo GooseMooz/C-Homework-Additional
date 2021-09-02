83f623848d5c0868f0048e85db7ab2bb99178d5b
GooseMooz <55840845+GooseMooz@users.noreply.github.com>
2021-09-02 16:44:34 +0300

    Add files via upload

---
+++ Task 1/main.c
@@ -0,0 +1,31 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+
+int main(void) {
+    double lambda, sum = 0, s = 0, div;
+    int a, i = 0;
+    scanf("%lf", &lambda);
+    if (lambda <= 0 || lambda >= 1) {
+        printf("Lambda bad\n");
+        return 0;
+    }
+
+    FILE* inp = fopen("data.txt", "r");
+    int counter = 0;
+    if (inp == NULL) {
+        printf("Can't open file\n");
+        fclose(inp);
+        return 1;
+    }
+    while (fscanf(inp, "%d", &a) == 1) {
+        sum += a;
+        sum *= lambda;
+        counter += 1;
+    }
+    div = (1 - lambda) * (1 - pow(lambda, counter));
+    s = div * sum;
+    fclose(inp);
+    printf("Answer: %lf", s);
+    return 505;
+}
