e8f53295786127ff77e07cf058224847d6acc967
GooseMooz <55840845+GooseMooz@users.noreply.github.com>
2021-09-23 16:18:30 +0300

    Update Task_2.c

--- Task 2/Task_2.c
+++ Task 2/Task_2.c
@@ -2,25 +2,34 @@
 #include "stdlib.h"
 #include "math.h"
 
-double fileopen(double x) {
+double fileopen(double x, double* summary, double* answer) {
     double a;
     double ans = 0;
-    FILE* inp = fopen("C:\\Users\\user\\CLionProjects\\MSU\\data.txt", "r");
+    double sum = 0;
+    double counter = 0;
+    FILE* inp = fopen("data.txt", "r");
     if (inp == NULL) {
         printf("Can't open file\n");
         fclose(inp);
         return 1;
     }
     while (fscanf(inp, "%lf", &a) == 1) {
-        ans += a * a * pow(x, a - 1);
+        sum += a * pow(x, counter);
+        if (counter != 0) {
+            ans += a * counter * pow(x, counter - 1);
+        }
+        counter += 1;
     }
+    *summary = sum;
+    *answer = ans;
     fclose(inp);
-    return ans;
+    return 0;
 }
 
 int main(void) {
-    double input;
+    double input, sum, der;
     scanf("%lf", &input);
-    double answer = fileopen(input);
-    printf("%lf", answer);
+    fileopen(input, &sum, &der);
+    printf("%lf \n", sum);
+    printf("%lf", der);
 }
